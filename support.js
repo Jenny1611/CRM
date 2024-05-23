e35dd215e7256198c104a170f5e29603baa80b60
Jenny1611 <tissinojennifer04@gmail.com>
2024-05-23 16:49:34 +0200

    fix support.js

--- support.js
+++ support.js
@@ -1,13 +1,11 @@
-console.log("ok");
+const email_opening = document.querySelectorAll(".email-box-unopened");
 
-const email_opening = document.getElementByClassName("email-box-unopened");
-const icon_path = document.getElementById("email_icon");
-icon_path.src = "/sidebar/email-aperta.png";
-
-email_opening.forEach(email => {
+email_opening.forEach((email) => {
     email.addEventListener('click', () => {
-        email.className("")
-        email_icon_path = icon_path;
-        email.className("email-box-opened")
-    })
+        email.classList.remove("email-box-unopened");
+        email.classList.add("email-box-opened");
+
+        const emailIcon = email.querySelector(".email-icon");
+        emailIcon.src = "/sidebar/email-aperta.png";
+    });
 });

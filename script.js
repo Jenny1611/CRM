4b9a8ef20dfb5b47840ff5213f3f3191d3a8aadf
Jenny1611 <tissinojennifer04@gmail.com>
2024-05-24 14:22:41 +0200

    modifica js

--- script.js
+++ script.js
@@ -240,23 +240,3 @@ const myDoughnutChart = new Chart(ctx, {
     }
 });
 */
-
-function allowDrop(event) {
-    event.preventDefault();
-}
-
-function drag(event) {
-    event.dataTransfer.setData("text", event.target.id);
-}
-
-function drop(event) {
-    event.preventDefault();
-    const data = event.dataTransfer.getData("text");
-    const draggedElement = document.getElementById(data);
-
-    if (event.target.className === "div-ondrop") {
-        event.target.appendChild(draggedElement);
-    } else if (event.target.className === 'div-drag') {
-        event.target.insertAdjacentElement('afterend', draggedElement);
-    }
-}
